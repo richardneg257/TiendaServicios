880f265213bc96508289f554e5ae65dce2c8f870
Richard Negron <richard.negron.257@gmail.com>
2023-01-11 00:18:43 -0500

    Add project files.

---
+++ TiendaServicios.Api.Autor/Migrations/20230111051055_AutorLibro_GradoAcademico.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaServicios.Api.Autor.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutorLibroGradoAcademico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AutorLibro",
+                columns: table => new
+                {
+                    AutorLibroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AutorLibroGuid = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutorLibro", x => x.AutorLibroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GradoAcademico",
+                columns: table => new
+                {
+                    GradoAcademicoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CentroAcademico = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FechaGrado = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AutorLibroId = table.Column<int>(type: "int", nullable: false),
+                    GradoAcademicoGuid = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GradoAcademico", x => x.GradoAcademicoId);
+                    table.ForeignKey(
+                        name: "FK_GradoAcademico_AutorLibro_AutorLibroId",
+                        column: x => x.AutorLibroId,
+                        principalTable: "AutorLibro",
+                        principalColumn: "AutorLibroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GradoAcademico_AutorLibroId",
+                table: "GradoAcademico",
+                column: "AutorLibroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GradoAcademico");
+
+            migrationBuilder.DropTable(
+                name: "AutorLibro");
+        }
+    }
+}
