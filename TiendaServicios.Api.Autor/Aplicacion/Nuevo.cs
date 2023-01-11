d61ad8db19d812f778caa7fb5e818c10004cfdf9
Richard Negron <richard.negron.257@gmail.com>
2023-01-11 00:35:43 -0500

    mediatr y capa de aplicacion para autor

---
+++ TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string? Nombre { get; set; }
+            public string? Apellido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly ContextoAutor contexto;
+
+            public Manejador(ContextoAutor contexto)
+            {
+                this.contexto = contexto;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var autorLibro = new AutorLibro()
+                {
+                    Nombre = request.Nombre,
+                    Apellido = request.Apellido,
+                    FechaNacimiento = request.FechaNacimiento
+                };
+
+                contexto.AutorLibro.Add(autorLibro);
+                var valor = await contexto.SaveChangesAsync();
+
+                if (valor > 0) return Unit.Value;
+
+                throw new Exception("No se pudo insertar el Autor del libro");
+            }
+        }
+    }
+}
